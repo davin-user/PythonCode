884fa39fb00f6d1e57d21a5fc2d235e4e7d29de7
ZH0385 <jl.zhang@zhrail.net>
2026-08-18 16:15:15 +0800

    type change

--- 基础语法/5.类型转换.py
+++ 基础语法/5.类型转换.py
@@ -5,10 +5,14 @@ str(x) 将x转换为字符串
 """
 # 任何类型都可以转换为字符串
 # 字符串内必须要有数字才能转换为数字类型
+# 浮点数转换为整形会丢失精度
 
 change_int_value = int('123')
 change_float_value = float(123)
 change_str_value = str(123)
+change_int_float_value = int(123.12)
+
+print(change_int_float_value)
 print(type(change_int_value))
 print(change_float_value)
 print(type(change_float_value))

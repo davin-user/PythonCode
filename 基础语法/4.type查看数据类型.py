3ed18cc95b503ca2b0368f39388920028daa3dee
ZH0385 <jl.zhang@zhrail.net>
2026-08-20 17:02:50 +0800

    add IO and data type

--- 基础语法/4.type查看数据类型.py
+++ 基础语法/4.type查看数据类型.py
@@ -5,16 +5,23 @@ type()查看变量类型，可以是字面量，也可以是变量
 # 查看字面量
 print(type(123))
 print(type(123.123))
-print(type('查看字面量'))
+print(type("查看字面量"))
 
-print('----------------')
+print("----------------")
 
 # 查看变量
 int_value = 123
 float_value = 123.45
-str_value = '查看字面量类型'
+str_value = "查看字面量类型"
 print(type(int_value))
 print(type(float_value))
 print(type(str_value))
 
-
+# 使用\转义'或者"
+print('I\'m "ok"!')
+# 布尔值
+print(True and True)
+print(True and False)
+print(True or False)
+print(not True)
+print(not False)
